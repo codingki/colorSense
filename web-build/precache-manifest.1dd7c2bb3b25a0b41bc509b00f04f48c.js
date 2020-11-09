283165982a89fb13409b4eae98aea060ef699cd8
Nur Fikri <codingki@gmail.com>
2020-11-09 23:35:24 +0700

    added web build

---
+++ web-build/precache-manifest.1dd7c2bb3b25a0b41bc509b00f04f48c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "9824d17ba5c973b7df96",
+    "url": "/static/js/app.fb7a80c8.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "e007a615b62e9603ae77c0ff0c2e8fe7",
+    "url": "/static/media/right.1faa29ca.wav"
+  },
+  {
+    "revision": "948ca6bcf4cc12a4f492fb5b9bcedb03",
+    "url": "/static/media/wrong.fd20d091.wav"
+  },
+  {
+    "revision": "d9467ad26e38e8c074779736a15711b7",
+    "url": "/static/media/icon.c1e48201.png"
+  },
+  {
+    "revision": "54f4663391fa64931260b06dce5db81f",
+    "url": "/static/media/splash.2fbe8bc5.png"
+  },
+  {
+    "revision": "2896124908215aa3d76c22c183f985ea",
+    "url": "/static/js/2.39a57329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84518828ce91c08a6a73",
+    "url": "/static/js/2.39a57329.chunk.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "b111c9885313f0224d7af0e973ff77d9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "3e777b8922e18610ff43f7bbbb6e0812",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c46699072e26d2ed098e20bed946fa5",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "b71d3585f2e1791436bb60585e540115",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "cf263410c885e4e7b803988560af74b1",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  }
+];
